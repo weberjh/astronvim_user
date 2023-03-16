4423be2a70dd0f1420e090dbf80e414e5a987a01
unknown <jenshweber81@gmail.com>
2023-03-16 21:54:30 +0100

    added nvim-sexp text obj keybinds in config.

--- plugins/vim-sexp-bundle.lua
+++ plugins/vim-sexp-bundle.lua
@@ -3,11 +3,25 @@ return {
   -- vim-sexp and vim-sexp-mappings-for-regular-people only in certain buffers
    {
       "guns/vim-sexp",
+      -- enabled = false,
       ft = { "clojure", "lisp", "fennel", "scheme", "janet" },
       dependencies = {
          "tpope/vim-sexp-mappings-for-regular-people",
          "tpope/vim-repeat",
       },
+      config = function()
+         vim.g.sexp_mappings = {
+            ["af"] = "sexp_outer_list",
+            ["if"] = "sexp_inner_list",
+            ["aF"] = "exp_outer_top_list",
+            ["iF"] = "sexp_inner_top_list",
+            ["as"] = "exp_outer_string",
+            ["is"] = "sexp_inner_string",
+            ["ae"] = "sexp_outer_element",
+            ["ie"] = "sexp_inner_element"
+         }
+      end,
+
       opts = {
          -- no opts
       },
